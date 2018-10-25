4206bd923817b83b804f4e725d035e64ed56640c
Kai <b10223052@yuntech.org.tw>
2018-10-25 10:04:35 +0800

    make Map calss method more elasticity

--- src/Map.js
+++ src/Map.js
@@ -2,34 +2,34 @@ export default class Map {
   constructor(path, graphics) {
     this.path = path
     this.graphics = graphics
-    this.drawGrid()
-    this.drawRoute()
+    this.drawGrid(graphics)
+    this.drawRoute(path, graphics)
   }
 
-  drawRoute() {
+  drawRoute(path, graphics) {
     //線的路徑
-    this.path.lineTo(96, -32);
-    this.path.lineTo(96, 164);
-    this.path.lineTo(480, 164);
-    this.path.lineTo(480, 544);
+    path.lineTo(96, -32);
+    path.lineTo(96, 164);
+    path.lineTo(480, 164);
+    path.lineTo(480, 544);
     //線的樣式
-    this.graphics.lineStyle(3, 0xffffff, 1);
+    graphics.lineStyle(3, 0xffffff, 1);
     //畫線
-    this.path.draw(this.graphics);
+    path.draw(graphics);
   }
-  drawGrid() {
+  drawGrid(graphics) {
     //畫格子的數量(數量決定可以放的炮塔)
-    this.graphics.lineStyle(1, 0x000ff, 0.8)
+    graphics.lineStyle(1, 0x000ff, 0.8)
     for (var i = 0; i < 8; i++) {
       //從座標(0,64)畫到(640,64)，以此類推
-      this.graphics.moveTo(0, i * 64)
-      this.graphics.lineTo(640, i * 64)
+      graphics.moveTo(0, i * 64)
+      graphics.lineTo(640, i * 64)
     }
     //從座標(64,0)畫到(64,512)，以此類推
     for (var j = 0; j < 10; j++) {
-      this.graphics.moveTo(j * 64, 0)
-      this.graphics.lineTo(j * 64, 512)
+      graphics.moveTo(j * 64, 0)
+      graphics.lineTo(j * 64, 512)
     }
-    this.graphics.strokePath()
+    graphics.strokePath()
   }
 }
